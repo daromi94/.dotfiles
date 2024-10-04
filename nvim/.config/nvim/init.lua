28b96cf4d6c21c472ab8ff444216b58ee14c21e5
daromi94 <davromero07@gmail.com>
2024-10-03 22:20:21 -0500

    feat: :wrench: set up telescope

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,3 +1,5 @@
+require("base")
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   vim.fn.system({
@@ -11,8 +13,4 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-local plugins = {}
-local opts = {}
-
-require("base")
-require("lazy").setup(plugins, opts)
+require("lazy").setup("plugins")
