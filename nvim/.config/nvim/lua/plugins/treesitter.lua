282f556dd8bd4ceb48d1ac391c6be5010e7cd85a
daromi94 <davromero07@gmail.com>
2024-10-05 22:04:00 -0500

    refactor: :fire: remove java, kotlin, and scala from treesitter.ensure_installed

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -5,7 +5,7 @@ return {
     config = function()
       local config = require("nvim-treesitter.configs")
       config.setup({
-        ensure_installed = { "lua", "java", "kotlin", "scala" },
+        ensure_installed = { "lua" },
         highlight = { enable = true },
         indent = { enable = true }
       })
