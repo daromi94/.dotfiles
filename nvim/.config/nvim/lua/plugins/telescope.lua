a4b130e416287e4463ee84f8e7bdf1cbcea66f07
daromi94 <davromero07@gmail.com>
2024-10-05 22:02:21 -0500

    feat: :wrench: add remap for telescope.git_files

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -6,8 +6,9 @@ return {
     config = function()
       require("telescope").setup({})
       local builtin = require("telescope.builtin")
-      vim.keymap.set("n", "<C-p>", builtin.find_files, {})
-      vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+      vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
+      vim.keymap.set("n", "<leader>pg", builtin.git_files, {})
+      vim.keymap.set("n", "<leader>ps", builtin.live_grep, {})
     end
   }
 }
