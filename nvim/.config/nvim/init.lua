3192613d0dc2a4c9f8529ed1dc74255f58f863b6
daromi94 <davromero07@gmail.com>
2025-11-22 22:01:05 -0500

    build: :fire: remove lazy install script

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,20 +1,9 @@
 require("main")
 require("remap")
 
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out,                            "WarningMsg" },
-      { "\nPress any key to exit..." },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
-end
-vim.opt.rtp:prepend(lazypath)
+-- Load Lazy
+local lazy_path = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
+vim.opt.rtp:prepend(lazy_path)
 
 require("lazy").setup("plugins")
