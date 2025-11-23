b548bd77527251c901299757c171f8e8468a32a9
daromi94 <davromero07@gmail.com>
2025-11-22 22:05:09 -0500

    style: :art: clean up telescope config and keymaps

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -4,20 +4,22 @@ return {
     tag = "v0.1.9",
     dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
-      require("telescope").setup({})
+      local telescope = require("telescope")
+
+      telescope.setup({})
 
       local builtin = require("telescope.builtin")
 
-      vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find files" })
-      vim.keymap.set("n", "<leader>fd", builtin.oldfiles, { desc = "Find recent files" })
-      vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Live grep" })
-      vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Find buffers" })
-      vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Find help" })
-      vim.keymap.set("n", "<leader>fc", builtin.git_files, { desc = "Git: Find files" })
-      vim.keymap.set("n", "<leader>gs", builtin.git_status, { desc = "Git: Status" })
-      vim.keymap.set("n", "<leader>gc", builtin.git_commits, { desc = "Git: Commits" })
-      vim.keymap.set("n", "<leader>gb", builtin.git_branches, { desc = "Git: Branches" })
-      vim.keymap.set("n", "<leader>gd", builtin.git_bcommits, { desc = "Git: File history" })
-    end
+      vim.keymap.set("n", "<leader>ff", builtin.find_files)
+      vim.keymap.set("n", "<leader>fd", builtin.oldfiles)
+      vim.keymap.set("n", "<leader>fg", builtin.live_grep)
+      vim.keymap.set("n", "<leader>fb", builtin.buffers)
+      vim.keymap.set("n", "<leader>fh", builtin.help_tags)
+      vim.keymap.set("n", "<leader>fc", builtin.git_files)
+      vim.keymap.set("n", "<leader>gs", builtin.git_status)
+      vim.keymap.set("n", "<leader>gc", builtin.git_commits)
+      vim.keymap.set("n", "<leader>gb", builtin.git_branches)
+      vim.keymap.set("n", "<leader>gd", builtin.git_bcommits)
+    end,
   }
 }
